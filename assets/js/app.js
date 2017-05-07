f9868007cb9c929fb5111a31274e2661c22b07cf
Pradip Kumar Jaiswal <pradip.k.jaiswal@gmail.com>
2017-05-07 14:32:34 +0530

    JAVASCRIPT(II) Assignments session-6

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Below code about Greatest Among 3 Numbers & displaying in console.
+var a=10, b=20, c=7;
+if (a>b && a>c){ 
+console.log(" A is greater ");
+}
+if (b>a && b>c){ 
+console.log("%c B is greater ", 'background: #18bf7f; color: #ffffff;');
+}
+else{ 
+console.log(" C is greater ");
+}
